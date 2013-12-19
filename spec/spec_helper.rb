b560a33bec664d80e2221eac83005670a8e6a876
Karolis Astrauka <astrauka@gmail.com>
2013-12-19 13:32:17 +0200

    setup travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,6 @@ RSpec.configure do |config|
 
   config.use_transactional_fixtures = false
   config.infer_base_class_for_anonymous_controllers = false
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.order = "random"
 
   config.include FactoryGirl::Syntax::Methods
