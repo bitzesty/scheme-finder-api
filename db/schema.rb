46e4b509591449c1cc7cacbd667f9445f0b8027f
Karolis Astrauka <astrauka@gmail.com>
2013-12-23 18:46:58 +0200

    update scaffolding, add schemes navigation

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131223092049) do
+ActiveRecord::Schema.define(version: 20131223115531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "schemes", force: true do |t|
+    t.boolean  "had_direct_interactions"
+    t.string   "employees_number_range_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
