54e0e1b8fbc8b83d905707e9ece3a933cba4dffc
Karolis Astrauka <astrauka@gmail.com>
2013-12-30 12:19:03 +0200

    remove unused spec

--- spec/factories/schemes.rb
+++ spec/factories/schemes.rb
@@ -1,5 +1,11 @@
 FactoryGirl.define do
   factory :scheme do
     had_direct_interactions { [false, true].sample }
+    location_ids { Location.ids.sample }
+    sector_ids { Sector.ids.sample }
+    commitment_length_ids { CommitmentLength.ids.sample }
+    activity_ids { Activity.ids.sample }
+    company_size_ids { CompanySize.ids.sample }
+    age_range_ids { AgeRange.ids.sample }
   end
 end
