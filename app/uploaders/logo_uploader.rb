bd03d5c36b165033d93ed310053e066d689c2c2a
Saulius Grigaliunas <saulius@ninja.lt>
2013-12-30 10:16:11 +0200

    Add fallback logo image

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -1,13 +1,17 @@
 class LogoUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
+  version :logo do
+    process resize_to_fill: [50, 50]
+  end
+
   def extension_white_list
     %w(jpg jpeg png)
   end
 
   def default_url
     ActionController::Base.helpers.asset_path(
-      "fallback/" + [version_name, "default.png"].compact.join('_')
+      "fallback/logo_default.png"
     )
   end
 end
