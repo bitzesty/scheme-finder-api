82bbbd4f2f34dfb6da81a609ae604e45cdc4960c
Saulius Grigaliunas <saulius@ninja.lt>
2013-12-31 10:16:45 +0200

    Do not expose cookie secret key

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SchemeFinderApi::Application.config.secret_key_base = '926330952cf6def0ef93f46326b9d044459df1d19ca68df7b04fc5902f042c52b13f9e024c6506b9079dc53c01000ac17cf4f2403460d2dbe2b2bdbf1c02cb95'
+SchemeFinderApi::Application.config.secret_key_base = ENV['COOKIE_SECRET_KEY'] || 'development'
