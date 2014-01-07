13a3381d7a82729ffcba04f8c38399cb34d13d6a
Saulius Grigaliunas <saulius@ninja.lt>
2014-01-07 10:57:32 +0200

    Add backend scope to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,42 +1,39 @@
 SchemeFinderApi::Application.routes.draw do
-  devise_for :users
+  scope '/backend' do
+    devise_for :users
 
-  get '/pages/about', to: 'pages#about', as: :about
+    get '/pages/about', to: 'pages#about', as: :about
 
-  namespace :backend do
-    resource  :dashboard
-    resources :users
-    resources :schemes do
-      collection {
-        get :unconfirmed
-      }
-    end
-
-    resources :feedbacks do
-      collection {
-        get :unapproved
-      }
-    end
-
-    root to: 'dashboards#show'
-  end
-
-  namespace :api do
-    namespace :v1 do
-      resources :schemes, only: [:index, :create] do
-        scope module: :schemes do
-          resources :feedbacks, only: [:index, :create]
-        end
+    namespace :backend do
+      resource  :dashboard
+      resources :users
+      resources :schemes do
+        collection {
+          get :unconfirmed
+        }
       end
 
-      resources :activities, only: [:index]
-      resources :age_ranges, only: [:index]
-      resources :commitment_lengths, only: [:index]
-      resources :company_sizes, only: [:index]
-      resources :locations, only: [:index]
-      resources :sectors, only: [:index]
-    end
-  end
+      resources :feedbacks do
+        collection {
+          get :unapproved
+        }
+      end
 
-  root to: 'pages#about'
+      root to: 'dashboards#show'
+    end
+
+    namespace :api do
+      namespace :v1 do
+        resources :schemes, only: [:index, :create]
+        resources :activities, only: [:index]
+        resources :age_ranges, only: [:index]
+        resources :commitment_lengths, only: [:index]
+        resources :company_sizes, only: [:index]
+        resources :locations, only: [:index]
+        resources :sectors, only: [:index]
+      end
+    end
+
+    root to: 'pages#about'
+  end
 end
