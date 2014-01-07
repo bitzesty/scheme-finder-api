896d5ea8ea73909b8098e2116643a7657df7a362
Saulius Grigaliunas <saulius@ninja.lt>
2014-01-07 12:46:56 +0200

    Default api format is json

--- config/routes.rb
+++ config/routes.rb
@@ -24,13 +24,15 @@ SchemeFinderApi::Application.routes.draw do
 
     namespace :api do
       namespace :v1 do
-        resources :schemes, only: [:index, :create]
-        resources :activities, only: [:index]
-        resources :age_ranges, only: [:index]
-        resources :commitment_lengths, only: [:index]
-        resources :company_sizes, only: [:index]
-        resources :locations, only: [:index]
-        resources :sectors, only: [:index]
+        scope defaults: { format: 'json' } do
+          resources :schemes, only: [:index, :create]
+          resources :activities, only: [:index]
+          resources :age_ranges, only: [:index]
+          resources :commitment_lengths, only: [:index]
+          resources :company_sizes, only: [:index]
+          resources :locations, only: [:index]
+          resources :sectors, only: [:index]
+        end
       end
     end
 
