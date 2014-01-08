e7023c76d2f48bdda17c2916e6baca7a4d06efbb
Saulius Grigaliunas <saulius@ninja.lt>
2014-01-08 10:07:21 +0200

    Use relative_url_root, not route scope
    Prevents double backend path in the url like /backend/backend/users..

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,41 @@
 SchemeFinderApi::Application.routes.draw do
-  scope Rails.application.config.relative_url_root do
-    devise_for :users
+  devise_for :users
 
-    get '/pages/about', to: 'pages#about', as: :about
+  get '/pages/about', to: 'pages#about', as: :about
 
-    scope module: 'backend' do
-      resource  :dashboard
-      resources :accounts
-      resources :schemes do
-        collection {
-          get :unconfirmed
-        }
-      end
-
-      resources :feedbacks do
-        collection {
-          get :unapproved
-        }
-      end
+  scope module: 'backend' do
+    resource  :dashboard
+    resources :accounts
+    resources :schemes do
+      collection {
+        get :unconfirmed
+      }
     end
 
-    namespace :api do
-      namespace :v1 do
-        scope defaults: { format: 'json' } do
-          resources :schemes, only: [:index, :create] do
-            scope module: :schemes do
-              resources :feedbacks, only: [:index, :create]
-            end
-          end
-          resources :activities, only: [:index]
-          resources :age_ranges, only: [:index]
-          resources :commitment_lengths, only: [:index]
-          resources :company_sizes, only: [:index]
-          resources :locations, only: [:index]
-          resources :sectors, only: [:index]
-        end
-      end
+    resources :feedbacks do
+      collection {
+        get :unapproved
+      }
     end
-
-    root to: 'backend/dashboards#show'
   end
+
+  namespace :api do
+    namespace :v1 do
+      scope defaults: { format: 'json' } do
+        resources :schemes, only: [:index, :create] do
+          scope module: :schemes do
+            resources :feedbacks, only: [:index, :create]
+          end
+        end
+        resources :activities, only: [:index]
+        resources :age_ranges, only: [:index]
+        resources :commitment_lengths, only: [:index]
+        resources :company_sizes, only: [:index]
+        resources :locations, only: [:index]
+        resources :sectors, only: [:index]
+      end
+    end
+  end
+
+  root to: 'backend/dashboards#show'
 end
